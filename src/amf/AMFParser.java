e35a2424fe452579aa3c21b333ee84d5a984bbcf
U-wspride-PC\wspride <wspride@wspride-PC.(none)>
2014-06-04 23:50:17 -0400

    refactor

--- src/amf/AMFParser.java
+++ src/amf/AMFParser.java
@@ -33,23 +33,23 @@ import org.w3c.dom.NodeList;
 public class AMFParser extends SimpleApplication{
 
 	private String filepath;
-        private Material mat;
-        private BitmapText txt;
-        boolean cursorCaptive = true;
+	private Material mat;
+	private BitmapText txt;
+	boolean cursorCaptive = true;
 
 	public static void main(String[] args){
 
 		File mFile = doFilePrompt();
-                
+
 
 		if(mFile != null){
 			AMFParser app = new AMFParser(mFile.getAbsolutePath());
-                        
+
 			app.setShowSettings(false);
 			AppSettings settings = configureSettings();
 			app.setSettings(settings);
 			app.start();
-                        
+
 		} else {
 			System.out.println("Attachment cancelled by user.");
 		}
@@ -66,41 +66,41 @@ public class AMFParser extends SimpleApplication{
 		settings.put("Samples", 4);
 		return settings;
 	}
-        
-  private void initKeys() {
-    // You can map one or several inputs to one named action
-    inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_J));
-    inputManager.addMapping("Right",   new KeyTrigger(KeyInput.KEY_K));
-    inputManager.addMapping("EX", new KeyTrigger(KeyInput.KEY_X));
-    inputManager.addMapping("P", new KeyTrigger(KeyInput.KEY_P));
-    inputManager.addListener(actionListener,"Left","Right","EX","P");
- 
-  }
 
-    private ActionListener actionListener = new ActionListener() {
-    public void onAction(String name, boolean isPressed, float tpf) {
-        if (name.equals("Right")) {
-            mat.getAdditionalRenderState().setWireframe(false);
-        }
-        if (name.equals("Left")) {
-            mat.getAdditionalRenderState().setWireframe(true);
-        }
-        if (name.equals("EX") && !isPressed) {
-            System.out.println("disppear");
-                txt.setAlpha(0);
-        }
-        if (name.equals("P")){
-            if(cursorCaptive){
-                flyCam.setDragToRotate(true);
-                cursorCaptive = false;
-            } else{
-                flyCam.setDragToRotate(false);
-                cursorCaptive = true;
-            }
-        }
-    }
-  };
-  
+	private void initKeys() {
+		// You can map one or several inputs to one named action
+		inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_J));
+		inputManager.addMapping("Right",   new KeyTrigger(KeyInput.KEY_K));
+		inputManager.addMapping("EX", new KeyTrigger(KeyInput.KEY_X));
+		inputManager.addMapping("P", new KeyTrigger(KeyInput.KEY_P));
+		inputManager.addListener(actionListener,"Left","Right","EX","P");
+
+	}
+
+	private ActionListener actionListener = new ActionListener() {
+		public void onAction(String name, boolean isPressed, float tpf) {
+			if (name.equals("Right")) {
+				mat.getAdditionalRenderState().setWireframe(false);
+			}
+			if (name.equals("Left")) {
+				mat.getAdditionalRenderState().setWireframe(true);
+			}
+			if (name.equals("EX") && !isPressed) {
+				System.out.println("disppear");
+				txt.setAlpha(0);
+			}
+			if (name.equals("P")){
+				if(cursorCaptive){
+					flyCam.setDragToRotate(true);
+					cursorCaptive = false;
+				} else{
+					flyCam.setDragToRotate(false);
+					cursorCaptive = true;
+				}
+			}
+		}
+	};
+
 	public static File doFilePrompt(){
 		JInternalFrame tempFrame = new JInternalFrame("Select your File");
 		//System.out.println("newProjectDialog components count : " + newProjectDialog.getContentPane().getComponentCount());
@@ -123,200 +123,191 @@ public class AMFParser extends SimpleApplication{
 	public AMFParser(String filepath){
 		this.filepath = filepath;
 	}
-        
-        public static Document getDocument(File mFile){
-            try {
-                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-                Document doc = dBuilder.parse(mFile);
-                doc.getDocumentElement().normalize();
-                return doc;
-            } catch (Exception ex) {
-                Logger.getLogger(AMFParser.class.getName()).log(Level.SEVERE, null, ex);
-                return null;
-            }
-        }
-        
-        public void populateVertices(NodeList nList, Vector3f[] vertices){
-            			for (int temp = 0; temp < nList.getLength(); temp++) {
-				Node oNode = nList.item(temp);
 
-				if (oNode.getNodeType() == Node.ELEMENT_NODE) {
+	public static Document getDocument(File mFile){
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(mFile);
+			doc.getDocumentElement().normalize();
+			return doc;
+		} catch (Exception ex) {
+			Logger.getLogger(AMFParser.class.getName()).log(Level.SEVERE, null, ex);
+			return null;
+		}
+	}
 
-					NodeList oList = oNode.getChildNodes();
+	public void populateVertices(NodeList nList, Vector3f[] vertices){
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node oNode = nList.item(temp);
 
-					int oLength = oList.getLength();
+			if (oNode.getNodeType() == Node.ELEMENT_NODE) {
 
-					float xVal=0,yVal=0,zVal=0;
+				NodeList oList = oNode.getChildNodes();
 
-					for(int i=0;i<oLength;i++){
-						Node pNode = oList.item(i);
-						String pName = pNode.getNodeName();
-						if(pName == "coordinates"){
-							Element eElement = (Element) pNode;
-							xVal = Float.parseFloat(getTagValue("x", eElement));
-							yVal = Float.parseFloat(getTagValue("y", eElement));
-							zVal = Float.parseFloat(getTagValue("z", eElement));
-                                                        vertices[temp] = new Vector3f(xVal, yVal, zVal);
-						}
+				int oLength = oList.getLength();
+
+				float xVal=0,yVal=0,zVal=0;
+
+				for(int i=0;i<oLength;i++){
+					Node pNode = oList.item(i);
+					String pName = pNode.getNodeName();
+					if(pName == "coordinates"){
+						Element eElement = (Element) pNode;
+						xVal = Float.parseFloat(getTagValue("x", eElement));
+						yVal = Float.parseFloat(getTagValue("y", eElement));
+						zVal = Float.parseFloat(getTagValue("z", eElement));
+						vertices[temp] = new Vector3f(xVal, yVal, zVal);
 					}
 				}
 			}
-            
-        }
-        
-        public void populateIndicesColors(NodeList mList, ArrayList<Float> mColors, ArrayList<Integer> mIndices){
-                                    for (int temp = 0; temp < mList.getLength(); temp++) {
+		}
 
-				Node mNode = mList.item(temp);
-                                
-				if (mNode.getNodeType() == Node.ELEMENT_NODE) {
+	}
 
-					Element eElement = (Element) mNode;
+	public void populateIndicesColors(NodeList mList, ArrayList<Float> mColors, ArrayList<Integer> mIndices){
+		for (int temp = 0; temp < mList.getLength(); temp++) {
 
-					int v1 = Integer.parseInt(getTagValue("v1", eElement));
-					int v2 = Integer.parseInt(getTagValue("v2", eElement));
-					int v3 = Integer.parseInt(getTagValue("v3", eElement));
-                                        try{
-                                            float r = Float.parseFloat(getTagValue("r", eElement));
-                                            float g = Float.parseFloat(getTagValue("g", eElement));
-                                            float b = Float.parseFloat(getTagValue("b", eElement));
-                                            float a = Float.parseFloat(getTagValue("a", eElement));
-                                            mColors.add(r);
-                                            mColors.add(g);
-                                            mColors.add(b);
-                                            mColors.add(a);
-                                            mColors.add(r);
-                                            mColors.add(g);
-                                            mColors.add(b);
-                                            mColors.add(a);
-                                            mColors.add(r);
-                                            mColors.add(g);
-                                            mColors.add(b);
-                                            mColors.add(a);
-                                        } catch(NullPointerException npe){
-                                            System.out.println("no color");
-                                        }
+			Node mNode = mList.item(temp);
 
-					mIndices.add(v1);
-					mIndices.add(v2);
-					mIndices.add(v3);
-                                        
+			if (mNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element eElement = (Element) mNode;
+
+				int v1 = Integer.parseInt(getTagValue("v1", eElement));
+				int v2 = Integer.parseInt(getTagValue("v2", eElement));
+				int v3 = Integer.parseInt(getTagValue("v3", eElement));
+				try{
+					float r = Float.parseFloat(getTagValue("r", eElement));
+					float g = Float.parseFloat(getTagValue("g", eElement));
+					float b = Float.parseFloat(getTagValue("b", eElement));
+					float a = Float.parseFloat(getTagValue("a", eElement));
+					mColors.add(r);
+					mColors.add(g);
+					mColors.add(b);
+					mColors.add(a);
+					mColors.add(r);
+					mColors.add(g);
+					mColors.add(b);
+					mColors.add(a);
+					mColors.add(r);
+					mColors.add(g);
+					mColors.add(b);
+					mColors.add(a);
+				} catch(NullPointerException npe){
+					System.out.println("no color");
 				}
+
+				mIndices.add(v1);
+				mIndices.add(v2);
+				mIndices.add(v3);
+
 			}
-        }
-        
-        public static NodeList getElements(File mFile, String mTag){
-            Document mDocument = getDocument(mFile);
-            return mDocument.getElementsByTagName(mTag);
-        }
+		}
+	}
+
+	public void setFinalVertices(ArrayList<Integer> mIndices, ArrayList<Integer> indices, Vector3f[] vertices, Vector3f newVertices[]){
+            
+		for(int i=0; i< mIndices.size(); i+=3){
+                    
+			Integer m = mIndices.get(i);
+			Integer n = mIndices.get(i+1);
+			Integer o = mIndices.get(i+2);
+			Vector3f oldVertexM = vertices[m.intValue()];
+			Vector3f oldVertexN = vertices[n.intValue()];
+			Vector3f oldVertexO = vertices[o.intValue()];
+			newVertices[i] = new Vector3f(oldVertexM.getX(), oldVertexM.getY(), oldVertexM.getZ());
+			newVertices[i+1] = new Vector3f(oldVertexN.getX(), oldVertexN.getY(), oldVertexN.getZ());
+			newVertices[i+2] = new Vector3f(oldVertexO.getX(), oldVertexO.getY(), oldVertexO.getZ());
+
+			indices.add(i);
+			indices.add(i+1);
+			indices.add(i+2);
+
+		}
+	}
+
+	public static NodeList getElements(File mFile, String mTag){
+		Document mDocument = getDocument(mFile);
+		return mDocument.getElementsByTagName(mTag);
+	}
 
 	public void simpleInitApp() {
-            System.out.println("derpy derp");
+
 		/** Create a pivot node at (0,0,0) and attach it to the root node */
 		com.jme3.scene.Node pivot = new com.jme3.scene.Node("pivot");
 		rootNode.attachChild(pivot); // put this node in the scene
 
 		Vector3f [] vertices;
-                
+
 		ArrayList<Integer> mIndices = new ArrayList<Integer>();
-                
-                ArrayList<Float> mColors = new ArrayList<Float>();
+
+		ArrayList<Float> mColors = new ArrayList<Float>();
 
 		Mesh mesh = new Mesh();
 
-		try{
+		File amfFile = new File(filepath);
 
-			File amfFile = new File(filepath);
-			NodeList nList = getElements(amfFile, "vertex");
-                        NodeList mList = getElements(amfFile,"triangle");
+		NodeList nList = getElements(amfFile, "vertex");
+		NodeList mList = getElements(amfFile,"triangle");
 
-			vertices = new Vector3f[nList.getLength()];
-                        
-                        populateIndicesColors(mList, mColors, mIndices);
+		vertices = new Vector3f[nList.getLength()];
 
-			populateVertices(nList, vertices);
-                        
-                        Vector3f [] newVertices = new Vector3f[mIndices.size()];
-                     
-                        ArrayList<Integer> indices = new ArrayList<Integer>();
-                        
-                        for(int i=0; i< mIndices.size(); i+=3){
-                            System.out.println(i);
-                            Integer m = mIndices.get(i);
-                            Integer n = mIndices.get(i+1);
-                            Integer o = mIndices.get(i+2);
-                            Vector3f oldVertexM = vertices[m.intValue()];
-                            Vector3f oldVertexN = vertices[n.intValue()];
-                            Vector3f oldVertexO = vertices[o.intValue()];
-                            newVertices[i] = new Vector3f(oldVertexM.getX(), oldVertexM.getY(), oldVertexM.getZ());
-                            newVertices[i+1] = new Vector3f(oldVertexN.getX(), oldVertexN.getY(), oldVertexN.getZ());
-                            newVertices[i+2] = new Vector3f(oldVertexO.getX(), oldVertexO.getY(), oldVertexO.getZ());
-                            
-                            System.out.println(newVertices[i]);
-                            
-                            indices.add(i);
-                            indices.add(i+1);
-                            indices.add(i+2);
+		populateIndicesColors(mList, mColors, mIndices);
 
-                            
-                        }
-                        
-                        int[] indexes = new int[indices.size()];
-                        for(int i=0; i<indices.size();i++){
-                            System.out.println("index "+ i + " " + indices.get(i));
-                            indexes[i] = indices.get(i);
-                        }
-                        
-                        System.out.println("indexes: " + indexes.length);
-                        System.out.println("colors: " + mColors.size());
-                        System.out.println("vertexes: " + newVertices.length);
-                        
-                        System.out.println("nv: " + newVertices);
+		populateVertices(nList, vertices);
 
-			mesh.setBuffer(VertexBuffer.Type.Position, 3, BufferUtils.createFloatBuffer(newVertices));
-			//mesh.setBuffer(Type.TexCoord, 2, BufferUtils.createFloatBuffer(texCoord));
-			mesh.setBuffer(VertexBuffer.Type.Index, 3, BufferUtils.createIntBuffer(indexes));
+		Vector3f [] newVertices = new Vector3f[mIndices.size()];
 
-			mesh.updateBound();
+		ArrayList<Integer> indices = new ArrayList<Integer>();
 
-			Geometry geo = new Geometry("OurMesh", mesh); // using our custom mesh object
-			mat = new Material(assetManager, 
-					"Common/MatDefs/Misc/Unshaded.j3md");
-                        
-                        if(mColors.size() > 0){
-                            float[] colors = new float[mColors.size()];
-                            for(int i=0;i<mColors.size(); i++) {
-                                colors[i] = mColors.get(i);
-                            }
-                            mesh.setBuffer(Type.Color, 4, colors);
-                            mat.setBoolean("VertexColor", true);
-                        } else{
-                            mat.setColor("Color", ColorRGBA.Blue);
-                        }
+		setFinalVertices(mIndices, indices, vertices, newVertices);
 
-			mat.getAdditionalRenderState().setWireframe(true);
-                  
-			geo.setMaterial(mat);
-			rootNode.attachChild(geo);
-                        
-                                BitmapFont fnt = assetManager.loadFont("Interface/Fonts/Default.fnt");
-        txt = new BitmapText(fnt, false);
-        txt.setBox(new Rectangle(0, 600, 400, 100));
-        txt.setSize(fnt.getPreferredSize());
-        txt.setText("Click and Drag to rotate camera. Use arrow keys to move camera. JK to toggle wireframe/solid. X to close this text.");
-        txt.setLocalTranslation(0, txt.getHeight(), 0);
-        guiNode.attachChild(txt);
-
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
+		int[] indexes = new int[indices.size()];
                 
-                initKeys();
-                flyCam.setDragToRotate(true);
-                flyCam.setMoveSpeed(7);
-                
+		for(int i=0; i<indices.size();i++){
+			indexes[i] = indices.get(i);
+		}
+
+		mesh.setBuffer(VertexBuffer.Type.Position, 3, BufferUtils.createFloatBuffer(newVertices));
+		//mesh.setBuffer(Type.TexCoord, 2, BufferUtils.createFloatBuffer(texCoord));
+		mesh.setBuffer(VertexBuffer.Type.Index, 3, BufferUtils.createIntBuffer(indexes));
+
+		mesh.updateBound();
+
+		Geometry geo = new Geometry("OurMesh", mesh); // using our custom mesh object
+		mat = new Material(assetManager, 
+				"Common/MatDefs/Misc/Unshaded.j3md");
+
+		if(mColors.size() > 0){
+			float[] colors = new float[mColors.size()];
+			for(int i=0;i<mColors.size(); i++) {
+				colors[i] = mColors.get(i);
+			}
+			mesh.setBuffer(Type.Color, 4, colors);
+			mat.setBoolean("VertexColor", true);
+		} else{
+			mat.setColor("Color", ColorRGBA.Blue);
+		}
+
+		mat.getAdditionalRenderState().setWireframe(true);
+
+		geo.setMaterial(mat);
+		rootNode.attachChild(geo);
+
+		BitmapFont fnt = assetManager.loadFont("Interface/Fonts/Default.fnt");
+		txt = new BitmapText(fnt, false);
+		txt.setBox(new Rectangle(0, 600, 400, 100));
+		txt.setSize(fnt.getPreferredSize());
+		txt.setText("Click and Drag to rotate camera. Use arrow keys to move camera. JK to toggle wireframe/solid. X to close this text.");
+		txt.setLocalTranslation(0, txt.getHeight(), 0);
+		guiNode.attachChild(txt);
+
+
+		initKeys();
+		flyCam.setDragToRotate(true);
+		flyCam.setMoveSpeed(7);
+
 	}
 	public String getTagValue(String sTag, Element eElement) {
 		NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
